0118c62b7299490fdd12f03df1732413cc7dc827
MahimnaBndari <mahimnabandari258@gmail.com>
2024-09-17 15:13:11 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,12 +1,1 @@
-#include "StringCalculator.h"
-#include <gtest/gtest.h>
- 
-TEST(StringCalculatorTestSuite,add_emptyInputString_ZeroIsExpected){
-//Arrange
-  string input="";
-  int expectedValue=0;
-//Act
-  int actualValue=Add(input);
-//Assert
-  ASSERT_EQ(actualValue,expectedValue);
-}
+int Add(const string &input);
