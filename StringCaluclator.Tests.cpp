9521496c056ab2142a6ff9dc952da808069cbe53
MahimnaBndari <mahimnabandari258@gmail.com>
2024-09-17 15:08:44 +0530

    Update StringCaluclator.Tests.cpp

--- StringCaluclator.Tests.cpp
+++ StringCaluclator.Tests.cpp
@@ -1,12 +1,58 @@
-#include "StringCaluclator.h"
-#include <gtest/gtest.h>
+#include "StringCalculator.h"
 
-TEST(StringCaluclatorTestSuite,add_emptyInputString_ZeroIsExpected){
-//Arrange
-  String input="";
-  int expectedValue=0;
-  //Act
-  int actualValue=Add(input);
+#include <gtest/gtest.h>
+ 
+TEST(StringCalculatorTestSuite,add_emptyInputString_ZeroIsExpected){
+
+//Arrays
+
+string input="";
+
+int expectedValue=0;
+
+//Act
+
+int actualValue=Add(input);
+
 //Assert
-  ASSERT_EQ(actualValue,expectedValue);
+
+ASSERT_EQ(actualValue,expectedValue);
+
 }
+
+TEST(StringCalculatorTestSuite,add_oneInputString_OneIsExpected){
+
+//Arrange
+
+string input="1";
+
+int expectedValue=1;
+
+//Act
+
+int actualValue=Add(input);
+
+//Assert
+
+ASSERT_EQ(actualValue,expectedValue);
+
+}
+
+TEST(StringCalculatorTestSuite,add_twoInputString_SumIsExpected){
+
+//Arrange
+
+string input="2,2";
+
+int expectedValue=4;
+
+//Act
+
+int actualValue=Add(input);
+
+//Assert
+
+ASSERT_EQ(actualValue,expectedValue);
+
+}
+ 
