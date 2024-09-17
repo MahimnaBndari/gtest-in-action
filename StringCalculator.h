abb7ba15268a9a1b213686cf06826038fc73638b
MahimnaBndari <mahimnabandari258@gmail.com>
2024-09-17 15:02:57 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,8 +1,12 @@
-#include <string>
-#include <vector>
-#include <numeric>
-#include <regex>
-using namespace std;
-
-//int Add9const char *input);
-int Add(const string &input);
+#include "StringCalculator.h"
+#include <gtest/gtest.h>
+ 
+TEST(StringCalculator.TestSuite,add_emptyInputString_ZeroIsExpected){
+//Arrange
+  string input="";
+  int expectedValue=0;
+//Act
+  int actualValue=Add(input);
+//Assert
+  ASSERT_EQ(actualValue,expectedValue);
+}
